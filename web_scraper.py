61c738efc47f1343fe9a91a9e19803364ea13593
Soul <ken.liu1@ucalgary.ca>
2024-03-30 21:39:10 -0600

    Added get_relics and a helper function to remove html attributes

--- web_scraper.py
+++ web_scraper.py
@@ -50,3 +50,74 @@ def get_character_info(page):
             playable_characters[key].append(character_info[key])
         
     return playable_characters, num_of_playable_characters
+
+def get_relics(page):
+    """
+    Returns the relics' information alongside the total number of relics
+
+    page -- The html page of the website
+    """
+    soup = BeautifulSoup(page, "html.parser")
+    relics = dict(set_icon=[], set_name=[], pieces_name=[], pieces_icon=[], bonuses=[])
+
+    # Gets only the specified html attribute using regex
+    pattern_src = r'src="(.*?)"'
+    pattern_title = r'title="(.*?)"'
+
+    # Gets total number of relics
+    total_relics = int(str(soup.find_all("b")[2]).replace("<b>", "").replace("</b>", ""))
+    
+    all_table_rows = soup.find_all("tr")
+    bonus_desc_html = soup.find_all(id="mw-customcollapsible-relicsettable")
+    
+    for index in range(1, total_relics + 1):
+        # Pattern to seperate the different tags/attributes
+        title_elements = re.findall(pattern=pattern_title, string=str(all_table_rows[index]))
+        src_elements = re.findall(pattern=pattern_src, string = str(all_table_rows[index]))
+
+        set_name = title_elements[0]
+        pieces_name = []
+
+        # Store all four different set piece names (helmet, gloves, body, shoes)
+        for piece_index in range(2, len(title_elements), 2):
+            if len(pieces_name) == 4:
+                break
+            else:
+                pieces_name.append(title_elements[piece_index])
+
+        set_icon = src_elements[0]
+        pieces_icon = []
+
+        # Store all four different set piece icons (helmet, gloves, body, shoes)
+        for piece_index in range(2, len(src_elements), 4):
+            pieces_icon.append(src_elements[piece_index])
+        
+        bonus_desc = _remove_html_attributes(bonus_desc_html[index])
+        
+        set_info = dict(set_icon=set_icon, set_name=set_name, pieces_name=pieces_name, pieces_icon=pieces_icon, bonuses=bonus_desc)
+
+        for key in relics:
+            relics[key].append(set_info[key])
+
+    return relics, total_relics
+
+
+def get_planars(page):
+    """
+    Returns the planars' information alongside the total number of planars
+
+    page -- The html page of the website
+    """
+    soup = BeautifulSoup(page, "html.parser")
+    planars = dict(icon=[], set=[], pieces=[], bonuses=[])
+
+def _remove_html_attributes(html):
+    """
+    A helper function to remove html attributes from a string, and return that string.
+
+    html -- HTML code
+    """
+    attribute_removal_pattern = r'<.*?>'
+    string = re.sub(pattern=attribute_removal_pattern, repl='' ,string=str(html))
+    
+    return string
